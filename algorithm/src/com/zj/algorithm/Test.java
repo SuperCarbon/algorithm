b23d392913791801b28b059284f22bb198501aea
周军 <izhoujun@163.com>
2013-06-01 18:46:09 +0800

    初始化算法练习项目
    Signed-off-by: 周军 <izhoujun@163.com>

---
+++ algorithm/src/com/zj/algorithm/Test.java
@@ -0,0 +1,16 @@
+package com.zj.algorithm;
+
+public class Test {
+
+	/**
+	 * @Title: main
+	 * @Description: TODO
+	 * @param args
+	 * @throws
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
