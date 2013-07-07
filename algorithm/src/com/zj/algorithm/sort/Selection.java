13409b779023aa711cb0658c6e52aa6f688bc21e
周军 <izhoujun@163.com>
2013-07-07 10:09:12 +0800

    加入选择排序

--- algorithm/src/com/zj/algorithm/sort/Selection.java
+++ algorithm/src/com/zj/algorithm/sort/Selection.java
@@ -2,14 +2,22 @@ package com.zj.algorithm.sort;
 
 public class Selection extends SortBase {
 
-	
 	public static void main(String[] args) {
-		Comparable[] arrayChar = { 'S', 'O', 'R', 'T', 'E', 'X', 'A', 'M', 'P', 'L',
-				'E' };
-		
-		show(arrayChar);
-		
+		Comparable[] arrayChar = { 'S', 'O', 'R', 'T', 'E', 'X', 'A', 'M', 'P',
+				'L', 'E' };
 
+		int length = arrayChar.length;
+		for (int i = 0; i < length; i++) {
+			int min = i;
+			for (int j = i; j < length; j++) {
+				if (!less(arrayChar[min], arrayChar[j])) {
+
+					min = j;
+				}
+			}
+			exchange(arrayChar, min, i);
+		}
+		show(arrayChar);
 	}
 
 }
